07c079724c4c6dfdf4f99f28dc510a0e25ad7e4b
starquize <kguqo@ptct.net>
2025-04-12 19:06:06 +0300

    finish

---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const SUB1 = urlParams.get('sub1') || ''; 
+const LOCALE_CODE = urlParams.get('locale_code') || 'en';
+const isAdm = urlParams.get('adm') || false;
+
+
+// Запрет на нажатия клавиш
+if (!isAdm) {
+    document.addEventListener('keydown', (e) => {
+        e.preventDefault();
+    });
+}
+
+
+// Дублируем параметры для ссылок  
+document.querySelectorAll(".site-nav").forEach(navElement => {
+    let url = navElement.getAttribute('href')
+    url += "?";
+            
+    // Добавляем параметры, если они есть
+    url += `&sub1=${SUB1}`;
+    url += `&locale_code=${LOCALE_CODE}`;
+
+    // Меняем ссылку на элементе
+    navElement.setAttribute("href", url.toString());
+});
