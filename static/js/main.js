31f8959ccf003b145856973598b8e5a84a15ef15
starquize <kguqo@ptct.net>
2025-04-17 22:17:43 +0300

    set default language

--- static/js/main.js
+++ static/js/main.js
@@ -2,9 +2,12 @@ const urlParams = new URLSearchParams(window.location.search);
 
 const SUB1 = urlParams.get('sub1') || ''; 
 const API_URL = urlParams.get("api_url");
-const LOCALE_CODE = urlParams.get('locale_code') || 'en';
 const DEPOSIT_URL = urlParams.get('deposit_url');
 const isAdm = urlParams.get('adm') || false;
+
+let localeCode = urlParams.get('locale_code');
+const availableLocales = ['ru', 'en', 'es'];
+const LOCALE_CODE = availableLocales.includes(localeCode) ? localeCode : 'ru';
 
 
 // Запрет на нажатия клавиш
